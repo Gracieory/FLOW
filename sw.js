d7f4a9a006b6e943d8847524989498988f925b41
Gracieory <gracyzhang0418@icloud.com>
2026-04-17 12:09:27 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flow-v7';
+const CACHE = 'flow-v8';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
